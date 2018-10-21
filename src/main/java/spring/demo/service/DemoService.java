190ee3ac4b39e508cf15252d04486e6a1730228e
qyb03220107 <34981586+qyb03220107@users.noreply.github.com>
2018-10-21 23:23:21 +0800

    Add files via upload

--- src/main/java/spring/demo/service/DemoService.java
+++ src/main/java/spring/demo/service/DemoService.java
@@ -8,8 +8,7 @@ public class DemoService {
 	public String Verify(String username,String password){
 		
 		if(username.equals("admin") && password.equals("admin")){
-			return "Login Success！ nihao
-			今天心情不好";
+			return "Login Success！ nihao 今天心情不好";
 		}else{
 			return "Login Failed,Please Try Again！";
 		}
