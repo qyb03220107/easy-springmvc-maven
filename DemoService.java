48f297c63373d7baa339cc30e882ebc26bfffb82
qyb03220107 <34981586+qyb03220107@users.noreply.github.com>
2018-10-21 23:12:09 +0800

    Add files via upload

---
+++ DemoService.java
@@ -0,0 +1,17 @@
+package spring.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoService {
+
+	public String Verify(String username,String password){
+		
+		if(username.equals("admin") && password.equals("admin")){
+			return "Login Success！ nihao
+			今天心情不好";
+		}else{
+			return "Login Failed,Please Try Again！";
+		}
+	}
+}
